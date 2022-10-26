e790f173dda2309f618498c80e083e1c843eec9e
philip <philip.jasionowski@gmail.com>
2022-10-26 10:30:18 -0400

    INITIAL COMIT RAHHHH

---
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//my maps api key (DONT USE BOZOS GET YOUR OWN FROM HERE:
+//https://console.cloud.google.com/google/maps-apis/start
+//AIzaSyCwLj_-la4UiknNMljtf5xCrAFbbkt3_h0
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
