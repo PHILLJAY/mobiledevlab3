beb0c798d3bc75965b1201b62a337826100f507b
Philip <philip.jasionowski@gmail.com>
2022-10-26 11:29:19 -0400

    Final commit

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.lab3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import  com.google.android.gms.maps.SupportMapFragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-//my maps api key (DONT USE BOZOS GET YOUR OWN FROM HERE:
-//https://console.cloud.google.com/google/maps-apis/start
-//AIzaSyCwLj_-la4UiknNMljtf5xCrAFbbkt3_h0
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    SupportMapFragment mapFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager();
-            findFragmentById(R.id.maps);
-            mapFragment.getMapAsync(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.94783269499836, -78.89829063926749)).title("OTU").snippet("Ontario Tech University"));
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(43.942563322959856, -78.89549033793327)).title("South Village").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.942563322959856, -78.89549033793327), 15));
     }
 }
